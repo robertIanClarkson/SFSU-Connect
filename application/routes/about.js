1e627d067741ae3e54244e7bac4d906be3527c33
Robert Clarkson <robert.ian.clarkson@gmail.com>
2020-09-11 02:12:39 -0400

    Made about/person tab-header more clean

--- application/routes/about.js
+++ application/routes/about.js
@@ -7,7 +7,8 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/:name', function(req, res, next) {
-  res.render(`about/${req.params.name}`, { title: req.params.name });
+  let name = req.params.name.charAt(0).toUpperCase() + req.params.name.slice(1); // capitalize name
+  res.render(`about/${req.params.name}`, { title: `About Us | ${name}` });
 })
 
 module.exports = router;
