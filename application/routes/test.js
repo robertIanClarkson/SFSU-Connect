2b231d017a74ee5c45a2d8373fe3899e063064a7
Robert Clarkson <robert.ian.clarkson@gmail.com>
2020-10-06 00:57:48 -0700

    DB Test - Cleaned up flow and added navigation buttons

--- application/routes/test.js
+++ application/routes/test.js
@@ -4,27 +4,27 @@ var test = require("./../db/helpers/test")
 
 /* GET test page. */
 router.get('/', function(req, res, next) {
-  res.render('test', { title: 'Test' });
-});
-
-router.post('/put', function(req, res, next) {
-  console.log("DATABASE PUT")
-  test.insert(req.body.name, req.body.message)
-    .then(() => {
-      res.render('test', { title: 'Test' });
-    })
-});
-
-router.post('/get', function(req, res, next) {
-  console.log("DATABASE GET")
   test.getAll()
     .then((rows) => {
       res.render('test', { 
-        title: 'Test', 
-        records: rows 
+        title: 'Test',
+        records: rows
       });
     })
-  
 });
+
+router.post('/put', function(req, res, next) {
+  test.insert(req.body.name, req.body.message)
+    .then(() => {
+      test.getAll()
+        .then((rows) => {
+          res.render('test', { 
+            title: 'Test',
+            records: rows
+          });
+        })
+    })
+});
+
 
 module.exports = router;
