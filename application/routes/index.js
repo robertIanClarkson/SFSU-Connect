456cfcd53ec65be6f97a363110f105d252931978
Robert Clarkson <robert.ian.clarkson@gmail.com>
2020-10-21 13:29:45 -0700

    Test page

--- application/routes/index.js
+++ application/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
+/* http://localhost:3000/test */
+router.get('/test', function(req, res, next) {
+  res.render('test', { title: 'Test' });
+});
+
 module.exports = router;
