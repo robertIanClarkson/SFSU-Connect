1fd556b9f67ec711f3e3a145ecfee2eaca62aac5
ChromatusX <jerryli@Jerrys-MacBook-Pro-2.local>
2020-10-23 17:57:06 -0700

    add inputs, need format and category

--- application/routes/index.js
+++ application/routes/index.js
@@ -7,8 +7,8 @@ router.get('/', function(req, res, next) {
 });
 
 /* http://localhost:3000/test */
-router.get('/test', function(req, res, next) {
-  res.render('test', { title: 'Test' });
+router.get('/postitem', function(req, res, next) {
+  res.render('postitem', { title: 'PostingItem' });
 });
 
 module.exports = router;
