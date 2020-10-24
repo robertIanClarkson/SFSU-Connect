f096b82512db6a0be0d3efe2a78b302987ed6e61
Taylor R Luke <tluke@mail.sfsu.edu>
2020-10-24 14:56:18 -0700

    taylor thankyouforposting

--- application/routes/index.js
+++ application/routes/index.js
@@ -7,8 +7,8 @@ router.get('/', function(req, res, next) {
 });
 
 /* http://localhost:3000/test */
-router.get('/test', function(req, res, next) {
-  res.render('test', { title: 'Test' });
+router.get('/thankyouforposting', function(req, res, next) {
+  res.render('thankyouforposting', { title: 'thankyouforposting' });
 });
 
 module.exports = router;
