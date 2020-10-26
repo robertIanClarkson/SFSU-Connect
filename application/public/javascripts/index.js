350e4c2a4b1797bbd33c1603ee09774b439dc5d4
Robert Clarkson <robert.ian.clarkson@gmail.com>
2020-10-25 19:01:41 -0700

    Frontend or VP done. Just need to make the DB call

--- application/public/javascripts/index.js
+++ application/public/javascripts/index.js
@@ -1,4 +1,61 @@
 /***********************DROPDOWN FOR VP**********************/
+var currentCategory = 'all'
+
 $('#category-dropdown-trigger').dropdown();
 
+$('#art-cat').click(function() {
+  currentCategory = 'art'
+  $('#category-dropdown-trigger').text(currentCategory)
+});
+
+$('#books-cat').click(function() {
+  currentCategory = 'books'
+  $('#category-dropdown-trigger').text(currentCategory)
+});
+
+$('#clothes-cat').click(function() {
+  currentCategory = 'clothes'
+  $('#category-dropdown-trigger').text(currentCategory)
+});
+
+$('#electronics-cat').click(function() {
+  currentCategory = 'electronics'
+  $('#category-dropdown-trigger').text(currentCategory)
+});
+
+$('#home-cat').click(function() {
+  currentCategory = 'home'
+  $('#category-dropdown-trigger').text(currentCategory)
+});
+
+$('#misc-cat').click(function() {
+  currentCategory = 'misc'
+  $('#category-dropdown-trigger').text(currentCategory)
+});
+
+$('#toys-cat').click(function() {
+  currentCategory = 'toys'
+  $('#category-dropdown-trigger').text(currentCategory)
+});
+
+$('#tutoring-cat').click(function() {
+  currentCategory = 'tutoring'
+  $('#category-dropdown-trigger').text(currentCategory)
+});
+
+$('#search-submit').click(function() {
+  var searchText = $('#search-input').val();
+
+  console.log(`Category: ${currentCategory}`)
+  console.log(`Text    : ${searchText}`)
+
+  searchData = {
+    category: currentCategory,
+    text: searchText
+  }
+
+  $.post( "search", searchData, function( data, status ) {
+    console.log(`Search data successfully sent --> (${status}:${data})`)
+  });
+});
 /***********************DROPDOWN FOR VP**********************/
--- application/routes/index.js
+++ application/routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('vp', { title: 'Home' });
 });
 
+router.post('/search', function(req, res, next) {
+  let searchData = req.body
+  console.log(searchData.category)
+  console.log(searchData.text)
+  res.sendStatus(200)
+});
+
 module.exports = router;
