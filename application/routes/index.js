654e7e1500b54f6d2163299dc838ffbe93c29b9c
Kevin <38668177+kefung2@users.noreply.github.com>
2020-10-31 17:43:49 -0700

    Kevin aboutv2 (#99)
    * Test page
    
    * v2 of the about page, mostly done, need to add a statement
    
    * added robert into
    
    * fix about page
    
    * fix all profile, this page should be done
    
    Co-authored-by: Robert Clarkson <robert.ian.clarkson@gmail.com>

--- application/routes/index.js
+++ application/routes/index.js
@@ -7,6 +7,10 @@ router.get('/', function(req, res, next) {
 });
 
 /* http://localhost:3000/test */
+router.get('/aboutv2', function(req, res, next) {
+    res.render('aboutv2', { title: 'Test' });
+});
+
 router.get('/test', function(req, res, next) {
     res.render('test', { title: 'Test' });
 });
