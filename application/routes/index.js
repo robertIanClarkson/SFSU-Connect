d37f7e8ef9611fe0ba9d65f4d1dfdd3b5b31233b
Taylor R Luke <tluke@mail.sfsu.edu>
2020-11-04 12:56:18 -0800

    added user: userData to index.js. changed account to p...Firstname

--- application/routes/index.js
+++ application/routes/index.js
@@ -51,7 +51,7 @@ router.get('/navbar', function(req, res, next) {
 
 /* http://localhost:3000/test */
 router.get('/account', function(req, res, next) {
-  res.render('account', { title: 'Account' });
+  res.render('account', { title: 'Account', user: userData });
 });
 
 /* http://localhost:3000/test */
