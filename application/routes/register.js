bdec95bd429e91efb19e755d675262afdde26342
Robert Clarkson <robert.ian.clarkson@gmail.com>
2020-11-06 12:48:52 -0800

    Registration working

--- application/routes/register.js
+++ application/routes/register.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var db = require('./../db/register')
 
 /* GET */
 router.get('/', function(req, res, next) {
@@ -10,7 +11,22 @@ router.get('/', function(req, res, next) {
 // PRIORITY 1
 router.post('/', function(req, res, next) {
   console.log(`POST: 'register' --> ${JSON.stringify(req.body)}`)
-  res.sendStatus(200)
+  db.newUser(req.body.first, req.body.last, req.body.email, req.body.password)
+    .then((result) => {
+      console.log(`register --> ${result}`)
+      res.render('landing', {
+        title: 'Home'
+      })
+    })
+    .catch((errno) => {
+      console.log(`Register Error: ${errno}`)
+      let error = "Error"
+      if(errno == 1062) { error = "That email address is taken, Sorry" }
+      res.render('register', {
+        title: 'Register',
+        error: error
+      })
+    })  
 });
 
 module.exports = router;
