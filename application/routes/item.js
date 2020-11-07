5ecd153c14b121d6c2f35a13c40e97e0d36901ea
Robert Clarkson <robert.ian.clarkson@gmail.com>
2020-11-06 23:39:24 -0800

    Forgot about 'thankyou' page

--- application/routes/item.js
+++ application/routes/item.js
@@ -16,16 +16,16 @@ router.get('/new', function(req, res, next) {
   }
 });
 
+router.get('/thankyou', function(req, res, next) {
+  res.render('thankyou', { title: 'Thanks!' });
+});
+
 router.get('/:id', function(req, res, next) {
   console.log(`GET: 'item/${req.params.id}'`)
   res.render('item', { title: 'Item' });  
 });
 
 
-
-// router.get('/thankyou', function(req, res, next) {
-//   res.render('thankyou', { title: 'Thanks!' });
-// });
 
 /* POST */
 // PRIORITY 1
