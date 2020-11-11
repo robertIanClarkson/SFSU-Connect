48ebfd7ca643f69d6b0624b71f33721d957e932f
camper4834 <camper4834@gmail.com>
2020-11-10 20:55:56 -0800

    Made the landing show latest 8 items

---
+++ application/db/index.js
@@ -0,0 +1,26 @@
+var db = require('./db')
+
+function getNItems(n) {
+  return new Promise((resolve, reject) => {
+    db.query(`SELECT item.name AS itemName, item.price, item.image, user.name AS userName
+              FROM item
+              JOIN user ON item.user_id = user.id
+              ORDER BY item.created DESC
+              LIMIT ?`, [n])
+      .then((rows) => {
+        if(rows.length == 0) {
+          reject(`(x) ERROR --> Database did not return any items`)
+        }
+        else {
+          resolve(rows)
+        }
+      })
+      .catch((err) => {
+        reject(`(${err}) ERROR --> DB call failed`)
+      })
+  });
+}
+
+module.exports = {
+  getNItems
+}
