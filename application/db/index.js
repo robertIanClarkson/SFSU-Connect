ba7cc4770627b7babf23aaef96c57e115d444260
camper4834 <camper4834@gmail.com>
2020-11-12 08:53:01 -0800

    Added logic to only pull items that were available

--- application/db/index.js
+++ application/db/index.js
@@ -5,6 +5,7 @@ function getNItems(n) {
     db.query(`SELECT item.name AS itemName, item.price, item.image, user.name AS userName
               FROM item
               JOIN user ON item.user_id = user.id
+              WHERE item.available = 1
               ORDER BY item.created DESC
               LIMIT ?`, [n])
       .then((rows) => {
