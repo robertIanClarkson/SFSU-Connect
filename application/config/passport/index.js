e04605ea042e99e43b4896c8ae3f6cc3853dfef6
Robert Clarkson <robert.ian.clarkson@gmail.com>
2020-11-14 13:41:10 -0800

    Encrypted login is working

--- application/config/passport/index.js
+++ application/config/passport/index.js
@@ -1,4 +1,5 @@
 const passport = require('passport');
+const bcrypt = require('bcrypt');
 const LocalStrategy = require('passport-local').Strategy;
 const login = require('./../../db/login')
 
@@ -14,26 +15,37 @@ passport.deserializeUser((user, done) => {
 
 // Login
 passport.use(new LocalStrategy({
-  usernameField: 'email',
-  passwordField: 'password'
-},
-function(username, password, done) {
-  let email = username // passport bullshit
-  login.getUserWithEmail(email)
-    .then((user) => {
-      if(user.password == password) {
-        delete user.password
-        console.log(user)
-        return done(null, user)
-      } else {
-        return done(null, false)
-      }
-    })
-    .catch((err) => {
-      console.log(err)
-      done(null, false)
-    })
-}
+    usernameField: 'email',
+    passwordField: 'password'
+  },
+  function(username, password, done) {
+    let email = username // passport bullshit
+    login.getUserWithEmail(email)
+      .then((user) => {
+        bcrypt.compare(password, user.password)
+          .then(function(isPassword) {
+            if(isPassword) {
+              delete user.password
+              // console.log(user)
+              return done(null, user)
+            } else {
+              return done(null, false)
+            }
+          })
+          .catch((error) => {
+            // failed to compare passwords
+            console.log(error)
+            done(null, false)
+          })
+      })
+      .catch((err) => {
+        // failed to query DB
+        console.log(err)
+        done(null, false)
+      })
+  }
 ));
+
+;
 
 module.exports = passport;
