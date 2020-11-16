8a5c076019ffc92d471a38bfbd70c032c10f1061
camper4834 <camper4834@gmail.com>
2020-11-15 20:58:09 -0800

    You can now only see items if they are available and approved

--- application/db/item.js
+++ application/db/item.js
@@ -3,7 +3,8 @@ var db = require('./db')
 function getItemByID(id) {
   return new Promise((resolve, reject) => {
     db.query(`SELECT * FROM item
-              WHERE id = ?`, [id])
+              WHERE id = ?
+              AND item.available = 1 AND item.approved = 1`, [id])
       .then((rows) => {
         if(rows.length == 0) {
           reject(`(x) ERROR --> Database did not return any items`)
