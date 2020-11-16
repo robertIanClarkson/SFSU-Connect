3b7e14ca8e96afb80da34a383b1fc7cb72fbd5fd
Robin <robert.ian.clarkson@gmail.com>
2020-11-15 19:21:12 -0800

    Revert "Encrypt password (#166)"
    This reverts commit 03725fac80bc592d3d52f6b050b8febe5e2adcdb.

--- application/config/passport/index.js
+++ application/config/passport/index.js
@@ -1,7 +1,8 @@
 const passport = require('passport');
-const bcrypt = require('bcrypt');
 const LocalStrategy = require('passport-local').Strategy;
 const login = require('./../../db/login')
+
+//set up database actions for user
 
 passport.serializeUser((user, done) => {
   done(null, user);
@@ -13,37 +14,26 @@ passport.deserializeUser((user, done) => {
 
 // Login
 passport.use(new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password'
-  },
-  function(username, password, done) {
-    let email = username // passport bullshit
-    login.getUserWithEmail(email)
-      .then((user) => {
-        bcrypt.compare(password, user.password)
-          .then((isPassword) => {
-            if(isPassword) {
-              delete user.password
-              // console.log(user)
-              return done(null, user)
-            } else {
-              return done(null, false)
-            }
-          })
-          .catch((error) => {
-            // failed to compare passwords
-            console.log(error)
-            done(null, false)
-          })
-      })
-      .catch((err) => {
-        // failed to query DB
-        console.log(err)
-        done(null, false)
-      })
-  }
+  usernameField: 'email',
+  passwordField: 'password'
+},
+function(username, password, done) {
+  let email = username // passport bullshit
+  login.getUserWithEmail(email)
+    .then((user) => {
+      if(user.password == password) {
+        delete user.password
+        // console.log(user)
+        return done(null, user)
+      } else {
+        return done(null, false)
+      }
+    })
+    .catch((err) => {
+      console.log(err)
+      done(null, false)
+    })
+}
 ));
-
-;
 
 module.exports = passport;
