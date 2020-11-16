1a9b1ab9d9e4d442aab55f731fb869b14a1418e0
Robert Clarkson <robert.ian.clarkson@gmail.com>
2020-11-15 17:35:51 -0800

    Register using 'none.png'

--- application/db/register.js
+++ application/db/register.js
@@ -2,9 +2,9 @@ var db = require('./db')
 
 function newUser(name, email, password) {
   let sqlCommand = `INSERT INTO user 
-                      (name, email, password) 
+                      (name, email, password, image) 
                       VALUES
-                      ('${name}', '${email}', '${password}');`
+                      ('${name}', '${email}', '${password}', 'none.png');`
   return new Promise((resolve, reject) => {
     db.query(sqlCommand)
       .then(() => {
