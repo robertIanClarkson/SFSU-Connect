9d353de0eeff61f57ec80cd3cdbe8669a58cc55b
camper4834 <camper4834@gmail.com>
2020-11-18 17:07:46 -0800

    BROKEN!!! First attempt to refactor the post(item/new) function. I'm so close but having issues with multer and scope of variables.

--- application/db/item.js
+++ application/db/item.js
@@ -1,8 +1,11 @@
-var db = require('./db')
+let item = require('./db');
+let multer = require('multer');
+let sharp = require('sharp');
+let crypto = require('crypto');
 
 function getItemByID(id) {
   return new Promise((resolve, reject) => {
-    db.query(`SELECT * FROM item
+    item.query(`SELECT * FROM item
               WHERE id = ?
               AND item.available = 1 AND item.approved = 1`, [id])
       .then((rows) => {
@@ -22,18 +25,18 @@ function getItemByID(id) {
 function newMessage(sender_id, item_id, message){
   return new Promise((resolve, reject) => {
       let getItem = `SELECT * FROM item WHERE id='${item_id}';`
-      db.query(getItem)
+      item.query(getItem)
           .then((items) => {
               console.log(items[0])
               let getReceiverUser = `SELECT * FROM user WHERE id='${items[0].user_id}';`
-              db.query(getReceiverUser)
+              item.query(getReceiverUser)
                   .then((receiver_users) => {
                       console.log(receiver_users[0])
                       let insertMessage = `INSERT INTO message  
                                     (item_id, user_id_sender, user_id_reciever, subject, message)
                                     VALUES
                                     ('${item_id}', '${sender_id}', '${receiver_users[0].id}', '${items[0].name}', '${message}');`
-                      db.query(insertMessage)
+                      item.query(insertMessage)
                           .then((result) => {
                               console.log('OK')
                               console.log(result)
@@ -47,17 +50,52 @@ function newMessage(sender_id, item_id, message){
   });
 }
 
-function newItem(name, description, price, category, fileName, userID){
+function insertItem(name, description, price, category, fileName, userID){
   let baseSQL = `INSERT INTO item (name, description, price, category_name, image, user_id) 
                   VALUES ('${name}', '${description}', '${price}', '${category}', '${fileName}', '${userID}')`
-  db.query(baseSQL)
+  let myPromise = item.query(baseSQL)
     .then(() =>{
       resolve('ok')
     })
     .catch((err) =>{
       reject(err.errno)
     })
+  
+  return myPromise;
 }
+
+let storage = multer.diskStorage({
+  destination: function(req, file, callback) {
+      callback(null, "public/images/items");
+  },
+  filename: function(req, file, callback) {
+      let fileExtension = file.mimetype.split("/")[1];
+      let randomName = crypto.randomBytes(16).toString("hex");
+      callback(null, `${randomName}.${fileExtension}`)
+  }
+});
+
+var myPromise;
+
+function newItem(req, res) {
+  let uploader = multer({storage: storage}).single('uploadImage');
+  uploader(req, res, ()=>{
+    let filePath = req.file.path;
+    let fileName = req.file.filename;
+    let thumbnailName = `thumbnail-${fileName}`;
+    let thumbnailPath = req.file.destination + "/" + thumbnailName;
+    let userID = req.user.id;
+    let name = req.body.itemname;
+    let description = req.body.description;
+    let price = req.body.price;
+    let category = req.body.category;
+  
+    sharp(filePath).resize(400).toFile(thumbnailPath);
+  
+    myPromise = insertItem(name, description, price, category, fileName, userID)
+  });
+  return myPromise;
+};
 
 module.exports = {
   getItemByID,
