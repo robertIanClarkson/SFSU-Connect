c2dfae5fb04bd556e5c29125791871fdbdea7b12
Robin <robert.ian.clarkson@gmail.com>
2020-11-20 13:50:30 -0800

    Fixed Upload Bug (#185)
    * Fixed Promise for newItem
    
    * Added error handling to POST 'item/new' route
    
    * Bugz
    
    * Updated makefile
    
    * Added revised insert item SQL script

--- application/db/item.js
+++ application/db/item.js
@@ -52,15 +52,16 @@ function newMessage(sender_id, item_id, message){
 
 function insertItem(name, description, price, category, fileName, userID){
     return new Promise(((resolve, reject) => {
-        let baseSQL = `INSERT INTO item (name, description, price, category_name, image, user_id) 
-                  VALUES ('${name}', '${description}', '${price}', '${category}', '${fileName}', '${userID}')`
-        item.query(baseSQL)
-            .then((myPromise) =>{
-                resolve(myPromise)
-            })
-            .catch((err) =>{
-                reject(err.errno)
-            })
+      let baseSQL = `INSERT INTO item (name, description, price, category_name, image, user_id) 
+                      VALUES (?, ?, ?, ?, ?, ?)`
+      item.query(baseSQL, [name, description, price, category, fileName, userID])
+        .then(() =>{
+          resolve('ok')
+        })
+        .catch((err) =>{
+          console.log('Failed at DB insert')
+          reject(err.errno)
+        })
     }))
 }
 
@@ -91,7 +92,13 @@ function newItem(req, res) {
 
             sharp(filePath).resize(400).toFile(thumbnailPath);
 
-            insertItem(name, description, price, category, fileName, userID).then((myPromise) => { resolve(myPromise)})
+            insertItem(name, description, price, category, fileName, userID)
+              .then((result) => { 
+                resolve('ok')
+              })
+              .catch((err) => {
+                reject(err)
+              })
         });
     }))
 
