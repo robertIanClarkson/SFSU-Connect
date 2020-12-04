abe0651646115dd5eafbccd6b58ac86f3f3a2b85
Gerardo Ochoa <gerardo.ochoa1@gmail.com>
2020-12-04 14:50:10 -0800

    change edit button on account page from redirect to opening form for submitting changes

---
+++ application/public/javascripts/account.js
@@ -0,0 +1,8 @@
+function openForm() {
+    document.getElementById("edit").style.display = "block";
+    document.getElementById("standard").style.display = "none";
+}
+function closeForm() {
+    document.getElementById("standard").style.display = "block";
+    document.getElementById("edit").style.display = "none";
+}
