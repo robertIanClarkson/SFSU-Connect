e85d05f4d915694d61f0076724fada04588272ee
camper4834 <camper4834@gmail.com>
2020-12-04 16:17:06 -0800

    Upload profile picture works on client side

--- application/public/javascripts/account.js
+++ application/public/javascripts/account.js
@@ -6,3 +6,15 @@ function closeForm() {
     document.getElementById("standard").style.display = "block";
     document.getElementById("edit").style.display = "none";
 }
+
+
+let uploadImage = document.getElementById('uploadImage');
+let targetImage = document.getElementById('accimg');
+uploadImage.addEventListener('change', (event) => {
+    let file = event.target.files[0];
+    let fileReader = new FileReader();
+    fileReader.onload = function(e) { 
+        targetImage.src = e.target.result;
+    }
+    fileReader.readAsDataURL(file);
+});
