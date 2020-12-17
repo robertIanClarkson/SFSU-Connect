3b64bef76055c2f80639752c77d4c7913a4eeb91
ChromatusX <jerryli@Jerrys-MacBook-Pro-2.local>
2020-12-16 20:30:38 -0800

    postitem display added

--- application/public/javascripts/newItem.js
+++ application/public/javascripts/newItem.js
@@ -2,4 +2,17 @@ $(document).ready(function(){
   $('select').formSelect();
 });
 
+function display(input){
+  if (input.files && input.files[0]) {
+    document.getElementById('holderpic').hidden = true;
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      $('#itemImg')
+        .attr('src', e.target.result)
+        .width(500)
+        .height(400);
+    };
 
+    reader.readAsDataURL(input.files[0]);
+  }
+}
